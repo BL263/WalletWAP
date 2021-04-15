90adaf667c1c20c19f800911109223dddc6bbc93
Mehrdad Kiani <mehrdad.kiani.eng@gmail.com>
2021-04-16 01:24:48 +0200

    import sql file improved

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,10 +3,10 @@ INSERT INTO `ecommerce`.`customer` (`id`, `delivery_address`, `email`, `name`, `
 INSERT INTO `ecommerce`.`customer` (`id`, `delivery_address`, `email`, `name`, `surname`) VALUES ('2', 'Italy', 'behnam.lotfi@studenti.polito.it', 'behnam', 'behi');
 INSERT INTO `ecommerce`.`wallet` (`id`, `amount`, `customer_id`) VALUES ('1', '1500', '1');
 INSERT INTO `ecommerce`.`wallet` (`id`, `amount`, `customer_id`) VALUES ('2', '1200', '2');
-INSERT INTO `ecommerce`.`transaction` (`id`, `amount_transferred`, `transaction_time`, `wallet_from`, `wallet_to`) VALUES ('1', '300', '2021-04-15 16:21:00', '1', '2');
-INSERT INTO `ecommerce`.`transaction` (`id`, `amount_transferred`, `transaction_time`, `wallet_from`, `wallet_to`) VALUES ('2', '400', '2021-03-15 16:21:00', '2', '1');
-INSERT INTO `ecommerce`.`transaction` (`id`, `amount_transferred`, `transaction_time`, `wallet_from`, `wallet_to`) VALUES ('3', '200', '2021-03-15 16:21:00', '1', '2');
-INSERT INTO `ecommerce`.`transaction` (`id`, `amount_transferred`, `transaction_time`, `wallet_from`, `wallet_to`) VALUES ('4', '250', '2021-02-15 16:21:00', '2', '1');
+INSERT INTO `ecommerce`.`transaction` (`id`, `amount_transferred`, `transaction_time`, `wallet_from_id`, `wallet_to_id`) VALUES ('1', '300', '2021-04-15 16:21:00', '1', '2');
+INSERT INTO `ecommerce`.`transaction` (`id`, `amount_transferred`, `transaction_time`, `wallet_from_id`, `wallet_to_id`) VALUES ('2', '400', '2021-03-15 16:21:00', '2', '1');
+INSERT INTO `ecommerce`.`transaction` (`id`, `amount_transferred`, `transaction_time`, `wallet_from_id`, `wallet_to_id`) VALUES ('3', '200', '2021-03-15 16:21:00', '1', '2');
+INSERT INTO `ecommerce`.`transaction` (`id`, `amount_transferred`, `transaction_time`, `wallet_from_id`, `wallet_to_id`) VALUES ('4', '250', '2021-02-15 16:21:00', '2', '1');
 
 
 

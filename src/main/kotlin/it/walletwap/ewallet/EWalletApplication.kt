46ade6cee18856176fe40ff2914af9f10909b9e0
Behnam Lotfi <behnam.lotfi@studenti.polito.it>
2021-04-29 20:33:15 +0200

    mail base added

--- src/main/kotlin/it/walletwap/ewallet/EWalletApplication.kt
+++ src/main/kotlin/it/walletwap/ewallet/EWalletApplication.kt
@@ -2,27 +2,83 @@ package it.walletwap.ewallet
 
 import it.walletwap.ewallet.domain.Customer
 import it.walletwap.ewallet.domain.Transaction
+import it.walletwap.ewallet.domain.User
 import it.walletwap.ewallet.domain.Wallet
 import it.walletwap.ewallet.dto.TransactionDTO
 import it.walletwap.ewallet.repositories.CustomerRepository
 import it.walletwap.ewallet.repositories.TransactionRepository
 import it.walletwap.ewallet.repositories.UserRepository
 import it.walletwap.ewallet.repositories.WalletRepository
+import it.walletwap.ewallet.services.impl.TransactionServiceImpl
+import it.walletwap.ewallet.services.impl.UserDetailsServiceImpl
 import it.walletwap.ewallet.services.impl.WalletServiceImpl
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.mail.SimpleMailMessage
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.mail.javamail.JavaMailSenderImpl
 import java.math.BigDecimal
 import java.text.SimpleDateFormat
 import java.util.*
-import it.walletwap.ewallet.Extensions
-import it.walletwap.ewallet.domain.User
-import it.walletwap.ewallet.services.impl.TransactionServiceImpl
-import it.walletwap.ewallet.services.impl.UserDetailsServiceImpl
+import javax.mail.MessagingException
+
 
 @SpringBootApplication
 class EWalletApplication :Extensions() {
+    @Value("\${spring.mail.host}")
+     val mailHost: String? = null
+    @Value("\${spring.mail.port}")
+    val mailPort: Int   ? = null
+    @Value("\${spring.mail.username}")
+    val mailUsername: String? = null
+    @Value("\${spring.mail.password}")
+    val mailPassword: String? = null
+    @Value("\${spring.mail.properties.mail.smtp.auth}")
+    val mailAuth: String? = null
+    @Value("\${spring.mail.properties.mail.smtp.starttls.enable}")
+    val mailEnable: String? = null
+    @Value("\${spring.mail.properties.mail.debug}")
+    val mailDebug: String? = null
+
+
+    fun sendMessage ( toMail:String, subject:String, mailBody:String){
+        var message= SimpleMailMessage()
+        message.setFrom(mailUsername.toString());
+        message.setTo(toMail)
+        message.setSubject(subject)
+        message.setText(mailBody)
+        message.setSentDate(Date())
+        getJavaMailSender()?.send(message)
+
+    }
+
+
+    fun getJavaMailSender(): JavaMailSender? {
+
+        val mailSender = JavaMailSenderImpl()
+        mailSender.host = mailHost
+        mailSender.port = mailPort!!
+        mailSender.username = mailUsername
+        mailSender.password = mailPassword
+        mailSender.protocol="smtp"
+
+        val javaMailProperties = Properties()
+        javaMailProperties["mail.smtp.auth"] = mailAuth
+        javaMailProperties["mail.smtp.starttls.enable"] = mailEnable
+        mailSender.javaMailProperties = javaMailProperties
+        try {
+            mailSender.testConnection()
+        }catch (e: MessagingException){
+            throw e
+        }
+
+        return mailSender
+    }
+
+
     @Bean
     fun test(
         customerRepo: CustomerRepository,
@@ -39,16 +95,9 @@ class EWalletApplication :Extensions() {
                 email = "andrea@gmail.com",
                 deliveryAddress = "roveda 29"
             )
-            val c3 = Customer(
-                name = "martina",
-                surname = "mancinelli",
-                email = "martina@gmail.com",
-                deliveryAddress = "roveda 29"
-            )
-            val c4 =
-                Customer(name = "irene", surname = "maldera", email = "irene@gmail.com", deliveryAddress = "roveda 29")
-
-            val user1= User(null,"martina1","pass",c3.email,true,Rolename.CUSTOMER.name)
+            val c3 = Customer(name = "martina", surname = "mancinelli", email = "martina@gmail.com", deliveryAddress = "roveda 29")
+            val c4 = Customer(name = "irene", surname = "maldera", email = "irene@gmail.com", deliveryAddress = "roveda 29")
+            val user1= User(null,"behnam2","pass","behnam263@gmail.com",true,Rolename.CUSTOMER.name)
             val c1Dto = c1.toDto()
             val c2Dto = c2.toDto()
             val c3Dto = c3.toDto()
@@ -105,6 +154,9 @@ class EWalletApplication :Extensions() {
             println(userService.getRoleName(user1))
             userService.registerUser(user1.toDto())
             println(userService.getuserByUserName(user1.username)?.email)
+
+           // sendMessage(user1.email.toString(),"Testing mail sender","Hi this is a test for mail sender")
+
         }
     }
 
